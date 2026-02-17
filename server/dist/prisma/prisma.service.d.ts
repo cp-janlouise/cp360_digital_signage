ed1dc82253d7ea2c841f5c9b9638e79c5124cd57
cp-janlouise <jan.catingub@cp-360.com>
2026-02-17 20:24:44 +0800

    Remove dist folder from tracking

--- server/dist/prisma/prisma.service.d.ts
+++
@@ -1,9 +0,0 @@
-import { OnModuleInit } from '@nestjs/common';
-declare const PrismaClient: any;
-export declare class PrismaService extends PrismaClient implements OnModuleInit {
-    private readonly client;
-    onModuleInit(): Promise<void>;
-    onModuleDestroy(): Promise<void>;
-    get prisma(): any;
-}
-export {};
